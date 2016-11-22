52ef8c7012c11e721cf6e561550e42a27b7b4ccc
Paulo J. Matos <paulojomatos@gmail.com>
2016-11-21 23:31:44 -0200

    game added to branch

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1479778124,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/bg-sheet0.png",
+		"images/ship-sheet0.png",
+		"images/trash-sheet0.png",
+		"images/asteroid-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/shield-sheet0.png",
+		"images/heart1-sheet0.png",
+		"images/asteroidexplosion-sheet0.png",
+		"media/asteroiddestroyed.ogg",
+		"media/firebullet.ogg",
+		"media/asteroid-destroyed1.ogg",
+		"media/asteroid-destroyed2.ogg",
+		"media/player-destroyed.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
